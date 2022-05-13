0d603508200a6d4a800ad0995248f80f643fbaba
Alexis Redd <alexisredd@reddithub.com>
2022-05-13 13:38:12 +0000

    add custom colors and icon lib to tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
       fontFamily: {
         'poppins': ['Poppins', 'sans-serif'],
       },
+      colors: {
+        'mern-dark': '#232227',
+        'mern-gray': '#333235',
+        'mern-red': '#ff626d',
+        'mern-orange': '#fcad72',
+      }
     }
   },
   plugins: [],
