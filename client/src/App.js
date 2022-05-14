36c485e2c54eb176b35cbddfdd08c2871b230db5
Apostolos Draganis <apdraganis@gmail.com>
2022-05-14 15:40:35 +0300

    Revert "Tidy up App.js"

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
-import TopBar from './components/NavBar/TopBar'
-import Header from './components/Header/Header';
-import ProductCategories from './components/Products/ProductCategories'
-import BottomBar from './components/Footer/BottomBar'
+import TopBar from './components/TopBar'
+import PromotionBanner from './components/PromotionBanner'
+import SearchBar from './components/SearchBar'
+import ProductCategories from './components/ProductCategories'
+import BottomBar from './components/BottomBar'
 
 function App() {
   return (
     <div className="flex flex-col bg-neutral-800 h-screen w-screen">
       <header>
         <TopBar />
-
+        <PromotionBanner />
       </header>
       <main>
-        <Header />
+        <SearchBar />
         <ProductCategories />
       </main>
       <footer>
