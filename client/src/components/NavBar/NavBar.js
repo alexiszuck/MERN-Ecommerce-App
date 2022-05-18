289440cdda1711db5e99ff04e44512857a4567cc
Apostolos Draganis <apdraganis@gmail.com>
2022-05-18 18:44:05 +0300

    Replace heroicons with @mui/icons-material

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,5 +1,7 @@
-import { GiftIcon } from '@heroicons/react/solid'
-import { ShoppingBagIcon, UserIcon, MenuAlt2Icon } from '@heroicons/react/outline'
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import MenuIcon from '@mui/icons-material/Menu';
+import RedeemIcon from '@mui/icons-material/Redeem';
 
 function NavBar() {
   const handleTopPromo = () => {
@@ -36,11 +38,11 @@ function NavBar() {
   return (
     <nav className="sticky top-0 z-50 flex flex-col items-center font-poppins">
       <div className="hidden sm:flex items-center justify-center bg-mern-gray h-8 w-full" onClick={handleTopPromo}>
-        <GiftIcon className="h-5 w-5 text-white mr-1 pb-1 cursor-pointer" /><span className="font-bold text-white text-xs cursor-pointer">FREE SHIPPING ON ORDER OF 50 DOLLARS</span>
+        <RedeemIcon className="h-5 w-5 text-white mr-1 pb-1 cursor-pointer" /><span className="font-bold text-white text-xs cursor-pointer">FREE SHIPPING ON ORDER OF 50 DOLLARS</span>
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-3 h-14 w-full text-white text-sm bg-gradient-to-r from-mern-red to-mern-orange shadow-mern-orange shadow-sm ">
         <div className="flex items-center justify-left sm:justify-center">
-          <MenuAlt2Icon className="h-6 pl-4 cursor-pointer sm:hidden" onClick={handleMenu} />
+          <MenuIcon className="sm:invisible ml-5 cursor-pointer" onClick={handleMenu} />
           <div className="hidden sm:block cursor-pointer text-mern-gray font-bold text-xl md:text-2xl">
             M <span className="font-normal">|</span> E <span className="font-normal">|</span> R <span className="font-normal">|</span> N
           </div>
@@ -54,10 +56,10 @@ function NavBar() {
         </div>
         <div className="flex items-center justify-end sm:justify-center space-x-4 pr-4 sm:pr-0">
           <div className="p-2 rounded-full bg-mern-gray cursor-pointer" onClick={handleCart} >
-            <ShoppingBagIcon className="h-5 text-white" />
+            <ShoppingBasketIcon className='h-5 text-white' />
           </div>
           <div className="p-2 rounded-full bg-mern-gray cursor-pointer" onClick={handleUser} >
-            <UserIcon className="h-5 text-white" />
+            <AccountCircleIcon className="h-5 text-white" />
           </div>
         </div>
       </div>
