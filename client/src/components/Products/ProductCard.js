c0a2c940230e11fb283c7f3e24f54a016ae48c05
alexisredd <105484653+alexisredd@users.noreply.github.com>
2022-05-22 19:21:28 +0700

    re-styling-product-card (#63)

--- client/src/components/Products/ProductCard.js
+++ client/src/components/Products/ProductCard.js
@@ -2,35 +2,56 @@ import StarBorderIcon from '@mui/icons-material/StarBorder';
 import StarIcon from '@mui/icons-material/Star';
 import StarHalfIcon from '@mui/icons-material/StarHalf';
 
-import Card from '../../ui/Card';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CardActions from '@mui/material/CardActions';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
-const ProductCard = ({ image, title, stars = 0, price = 0 }) => {
-  const productName = title && (
-    <div className='font-bold text-xl mb-2'>{title}</div>
-  );
-  const starArr = [1, 2, 3, 4, 5];
+const ProductCard = ({ image = 'https://picsum.photos/300/200', name = '', desc = '', stars = 0, price = 0 }) => {
   return (
-    <Card
-      image={image}
-      body={
-        <>
-          {productName}
-          {starArr.map((starNumber, index) => {
-            let [digit, frac] = parseFloat(stars).toFixed(1).split('.');
-            digit = parseInt(digit);
-            frac = parseInt(frac);
-            if (digit === starNumber && frac === 0)
-              return <StarIcon key={index} />;
-            if (digit === index && frac > 0)
-              return <StarHalfIcon key={index} />;
+    <Card sx={{ maxWidth: 300 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="200"
+          image={image}
+          alt="picsum"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {desc}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions disableSpacing>
+        <Stack sx={{ width: '100%' }} direction="row" justifyContent="space-between" alignItems="center">
+          <Stack direction="row" justifyContent="space-between">
+            {
+              [...Array(5).keys()].map(n =>
+                n < stars ?
+                  stars - n >= 1 ?
+                    <StarIcon key={`star-${n}`} sx={{ fontSize: 18 }} />
+                    :
+                    <StarHalfIcon key={`star-${n}`} sx={{ fontSize: 18 }} />
+                  :
+                  <StarBorderIcon key={`star-${n}`} sx={{ fontSize: 18 }} />
+              )
+            }
+          </Stack>
+          <Button size="small" color="primary">
+            DETAIL
+          </Button>
+        </Stack>
 
-            if (digit > index) return <StarIcon key={index} />;
-            return <StarBorderIcon key={index} />;
-          })}
-        </>
-      }
-      footer={<span className="text-sm">Starting Price: ${price} </span>}
-    />
+      </CardActions>
+    </Card>
   );
 };
 
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
-
+import Stack from '@mui/material/Stack';
 import HomeHeader from '../../components/modules/home/HomeHeader';
 import ProductCategories from '../../components/Products/ProductCategories';
+import ProductCard from '../../components/Products/ProductCard';
 
 const Home = (props) => {
+  const products = [
+    { product_id: 1, category: "Fashion/Shoe", brand: "Nike", name: "Nike Waffle One", desc: "Bringing a new look to the iconic Waffle franchise, the Nike Waffle One balances everything you love most about heritage Nike running with fresh innovations", image: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/fd9c5b95-2a2e-4b8d-b9e0-62dafab6b32f/waffle-one-mens-shoes-4cW37x.png", price: 100, stars: 4.3 },
+    { product_id: 2, category: "Test", brand: "Unknown", name: "NoName", desc: "no where", image: "https://picsum.photos/300/200", price: 345, stars: 1.2 }
+  ];
+
   return (
-    <div className='flex flex-col gap-14 sm:gap-4'>
+    <Stack spacing={{ xs: 10, sm: 1 }}>
       <HomeHeader />
       <ProductCategories />
-    </div>
+      {
+        products.map((item, idx) => (
+          <ProductCard key={`pd-${item.product_id}`} image={item.image} name={item.name} desc={item.desc} stars={item.stars} price={item.price} />
+        ))
+      }
+
+    </Stack>
   );
 };
 
