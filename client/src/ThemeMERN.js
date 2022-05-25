1c788bb843dabdb4a3aac4317ae7ca94db89a14c
alexisredd <105484653+alexisredd@users.noreply.github.com>
2022-05-25 09:18:11 +0700

    applying background color (#68)

--- client/src/ThemeMERN.js
+++ client/src/ThemeMERN.js
@@ -7,11 +7,24 @@ const ThemeMERN = createTheme({
             'sans-serif',
         ].join(','),
     },
-    // palette: {
-    //     primary: {
-    //         main: '#ff626d',
-    //     },
-    // },
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#ff626d',
+        },
+        secondary: {
+            main: '#fcad72',
+        },
+        merndark: {
+            main: '#232227',
+        },
+        merngrey: {
+            main: '#333235',
+        },
+        background: {
+            default: '#232227',
+        }
+    },
 });
 
 export default ThemeMERN;
